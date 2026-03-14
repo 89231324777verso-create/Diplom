f6228362b86bd2c0843e01c28636836a132dbd55
89231324777verso-create <89231324777verso@gmail.com>
2026-03-14 17:31:31 +0700

    add apitest

---
+++ apitest/configuration.py
@@ -0,0 +1,4 @@
+URL_SERVICE = "https://7dca0a93-95ae-4716-874f-ae0458bb51be.serverhub.praktikum-services.ru"
+
+CREATE_ORDER_PATH = "/api/v1/orders"
+GET_ORDER_BY_TRACK_PATH = "/api/v1/orders/track"
---
+++ apitest/sender_stand_request.py
@@ -0,0 +1,16 @@
+import requests
+import configuration
+
+
+def post_new_order(body):
+    return requests.post(
+        configuration.URL_SERVICE + configuration.CREATE_ORDER_PATH,
+        json=body
+    )
+
+
+def get_order_by_track(track):
+    return requests.get(
+        configuration.URL_SERVICE + configuration.GET_ORDER_BY_TRACK_PATH,
+        params={"t": track}
+    )
